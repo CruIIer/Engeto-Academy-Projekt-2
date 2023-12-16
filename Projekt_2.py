d4059315bebfdc9eff162efe0bd380fe0cf18b15
Cruller <apreclik@centrum.cz>
2023-12-16 13:07:28 +1000

    New file, Title, Introduction

---
+++ Projekt_2.py
@@ -0,0 +1,22 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+autor: Aleš Preclík
+email: apreclik@centrum.cz
+discord: alda_p
+"""
+
+
+from random import randint
+###### INTRODUCTION ######
+separator = 47 * "-"
+print(separator)
+print("Hi there!")
+print(separator)
+print("""I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.""")
+print(separator)
+    
+
+
+
