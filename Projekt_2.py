2091f3f8ab0e42cc1d4276ba3c9b3ed4cf4d94f4
Cruller <apreclik@centrum.cz>
2024-01-19 17:18:35 +1300

    Vyřešené globální proměnné + zabalení do funkcí

--- Projekt_2.py
+++ Projekt_2.py
@@ -19,51 +19,61 @@ print(separator)
     
     
 ###### CREATING 4 DIGITS result_pin CODE ######
-result_pin = []
-result_pin.append(randint(1,9))
-while len(result_pin) != 4:
-    num = randint(0, 9)
-    if num not in result_pin:
-        result_pin.append(num)
+def generate_pin():
+    result_pin = []
+    result_pin.append(randint(1,9))
+    while len(result_pin) != 4:
+        num = randint(0, 9)
+        if num not in result_pin:
+            result_pin.append(num)
+    return result_pin
 
-print(result_pin)
 
 ###### USER'S result_pin ######
-while True:
-    user_pin = input("Enter your number: ")
-    if len(user_pin) != 4:
-        print("Your result_pin must contain 4 digits.")
-    if user_pin[0] == "0":
-        print("Your result_pin can't start with 0.")
-    if not user_pin.isnumeric():
-        print("Your result_pin must contian numbers only.")
-    if len(set(user_pin)) != len(user_pin):
-        print("Your result_pin can't contain duplicates.")
-    else:
-        break
-
+def users_attempt_pin():
+    while True:
+        user_pin = input("Enter your number: ")
+        if len(user_pin) != 4:
+            print("Your result_pin must contain 4 digits.")
+        if user_pin[0] == "0":
+            print("Your result_pin can't start with 0.")
+        if not user_pin.isnumeric():
+            print("Your result_pin must contian numbers only.")
+        if len(set(user_pin)) != len(user_pin):
+            print("Your result_pin can't contain duplicates.")
+        else:
+            return user_pin
+    
 print(separator)
 
 ###### RESULTS OF A ROUND ######
-user_pin = [int(num) for num in user_pin]
-bulls = 0
-cows = 0
+def round_results():
+    result_pin = generate_pin()
+    user_pin = [int(num) for num in user_pin]
+    bulls = 0
+    cows = 0
+    
+    for num_pc, num_user in zip(result_pin, user_pin):
+        if num_pc in user_pin:
+            if num_pc == num_user:
+                bulls += 1
+            else:
+                cows += 1
+    return result_pin, user_pin, bulls, cows
 
-for num_pc, num_user in zip(result_pin, user_pin):
-    if num_pc in user_pin:
-        if num_pc == num_user:
-            bulls += 1
-        else:
-            cows += 1
 
 
-print(result_pin)
-print(user_pin)
+ 
 
-print(f"{bulls} bulls, {cows} cows") 
 
-# i gotta dostat user_pin do seznamu čísel, asi skrz comprehensions
-# 
+
+
+
+
+
+
+
+ 
 
 
 
