c164d5cdcea6179c13982976f6cbd17bdedaf627
Cruller <apreclik@centrum.cz>
2024-01-22 22:23:48 +1300

    Zacyklení kola, doladění detailů

--- Projekt_2.py
+++ Projekt_2.py
@@ -8,16 +8,14 @@ discord: alda_p
 
 
 from random import randint
-###### INTRODUCTION ######
 separator = 47 * "-"
 print(separator)
 print("Hi there!")
 print(separator)
 print("""I've generated a random 4 digit number for you.
 Let's play a bulls and cows game.""")
-print(separator)
-    
-    
+print(separator) 
+
 ###### CREATING 4 DIGITS result_pin CODE ######
 def generate_pin():
     result_pin = []
@@ -29,56 +27,50 @@ def generate_pin():
     return result_pin
 
 
-###### USER'S result_pin ######
+###### USER'S PIN ######
 def users_attempt_pin():
     while True:
         user_pin = input("Enter your number: ")
         if len(user_pin) != 4:
-            print("Your result_pin must contain 4 digits.")
-        if user_pin[0] == "0":
-            print("Your result_pin can't start with 0.")
-        if not user_pin.isnumeric():
-            print("Your result_pin must contian numbers only.")
-        if len(set(user_pin)) != len(user_pin):
-            print("Your result_pin can't contain duplicates.")
+            print("Your number must contain 4 digits.")
+        elif user_pin[0] == "0":
+            print("Your number can't start with 0.")
+        elif not user_pin.isnumeric():
+            print("Your number must contian numbers only.")
+        elif len(set(user_pin)) != len(user_pin):
+            print("Your number can't contain duplicates.")
         else:
-            return user_pin
+            return [int(num) for num in user_pin]
     
-print(separator)
+
 
 ###### RESULTS OF A ROUND ######
-def round_results():
-    result_pin = generate_pin()
-    user_pin = [int(num) for num in user_pin]
+result_pin = generate_pin()
+def round_results(result_pin):
+    user_pin = users_attempt_pin()
     bulls = 0
     cows = 0
     
     for num_pc, num_user in zip(result_pin, user_pin):
-        if num_pc in user_pin:
-            if num_pc == num_user:
-                bulls += 1
-            else:
-                cows += 1
+        if num_pc == num_user:
+            bulls += 1
+        elif num_user in result_pin:
+            cows += 1
     return result_pin, user_pin, bulls, cows
 
 
+###### SHOWING RESULTS ######
+guess_count = 0
+def show_results(result_pin, user_pin, bulls, cows, guess_count):
+    print(f"{bulls} {'bull' if bulls == 1 else 'bulls'}, {cows} {'cow' if cows == 1 else 'cows'} \n")
+    if bulls == 4:
+        print(f"Correct, you've guessed the right number in {guess_count} guesses")
 
- 
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
+while True:
+    result_pin, user_pin, bulls, cows = round_results(result_pin)
+    show_results(result_pin, user_pin, bulls, cows, guess_count)
+    guess_count += 1
+    if bulls == 4:
+        break
 
 
